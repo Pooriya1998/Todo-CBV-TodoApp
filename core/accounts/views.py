40743a8cc0ed22914bbe277424cc41633b70b9d5
Pooriya Zarasvandi Rahimzadeh <98259649+Pooriya1998@users.noreply.github.com>
2023-09-20 15:01:18 +0330

    Added registration section
    Changes made:
    
    1. Add registration section

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import FormView
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.shortcuts import redirect
 from django.contrib.auth import login
 from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Logout_View(LogoutView):
@@ -21,17 +24,17 @@ class Login_View(LoginView):
 
 class Register_View(FormView):
     template_name = "accounts/register.html"
-    form_class = UserCreationForm
-    success_url = reverse_lazy("post_list")
+    form_class = SignUpForm
+    success_url = reverse_lazy("todo:post_list")
     redirect_authenticated_user = True
 
     def form_valid(self, form):
         user = form.save()
         if user is not None:
-            login(self.request, user)
+            return redirect("accounts:login")
         return super(Register_View, self).form_valid(form)
 
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
-            return redirect("post_list")
+            return redirect("todo:post_list")
         return super(Register_View, self).get(*args, **kwargs)
